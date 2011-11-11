171642d4b3b00d2a7adc57277af64c8f206e3818
Paul Benson <xantarnet@gmail.com>
2011-11-11 12:27:39 -0800

    Initial Deploy of Rails 3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsProject::Application.config.secret_token = '3006d97e1c89b0cf531bd5709da9c09e67b3a13d399be8db48cc35e28bdf49ac7f6e609c1f2ee181c9f8b8055e11daccdd028a74170d531fafa73bdb06905817'
